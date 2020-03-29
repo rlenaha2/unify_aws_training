f04ec3c9ad46a7be06efba5871b856346695b055
Ryan <rlenaha2@gmail.com>
2020-03-29 12:30:17 -0700

    Adding a history folder to keep old runs and adding a dynamodb table

--- code/lambda_pandas_scrapper.py
+++ code/lambda_pandas_scrapper.py
@@ -42,13 +42,9 @@ def lambda_handler(event, context):
     
     soup = BeautifulSoup(page.content, 'html.parser')
     
-    whitelist = [
-      'p'
-    ]
+    whitelist = ['p' ]
     
     text_elements = [t for t in soup.find_all(text=True) if t.parent.name in whitelist]
-    #text_elements = list(map(lambda x:x.strip(),text_elements))
-    #text_elements = list(map(lambda x:x.replace("/n", ""),text_elements))
 
     text_elements = ' '.join(text_elements)
     text_elements = re.sub('\n', '', text_elements)
@@ -60,6 +56,6 @@ def lambda_handler(event, context):
         text_file.write(text_elements)
     
 
-    s3.Bucket(s3_bucket_name).upload_file('/tmp/Output.txt', 'raw_text')    
+    s3.Bucket(s3_bucket_name).upload_file('/tmp/Output.txt', 'text/raw_text')    
            
-
+    return URL2
